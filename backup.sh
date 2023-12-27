6b5b82400e8265f06fd4478b10fe2f72bc38d1bd
Salle de Lavage <salledelavager@gmail.com>
2023-12-27 00:22:32 -0500

    save

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ cp -f i3/config i3/config.bak
 echo "i3 backup taken"
 
 # i3status
-cp -f i3/i3status/config i3/i3status/config
+cp -f i3status/config i3status/config
 echo "i3status backup taken"
 
 # kitty
